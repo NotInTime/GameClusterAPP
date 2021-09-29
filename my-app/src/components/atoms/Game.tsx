9eb908a20601d43d3ff6e34627d36b4791f72764
Tobias <bytex.tobi@gmail.com>
2021-09-30 00:34:33 +0200

    Project

---
+++ my-app/src/components/atoms/Game.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+interface IGame {
+  Title: string;
+  ImageURL?: string;
+  Genre: string;
+}
+
+const Game: React.FC<IGame> = () => {
+  return <> </>;
+};
+
+export default Game;
