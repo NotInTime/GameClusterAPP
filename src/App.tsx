5aabd3f84fe4ddbb0f7e177f04239ba9fe1bb655
Tobias <bytex.tobi@gmail.com>
2021-11-06 15:23:22 +0100

    Fixed Sidebar Nav and Overlay in Total

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,16 @@
+import React from "react";
 import "./App.css";
+import Sidebar from "./components/Sidebar";
 import { GameContextProvider } from "./contexts/games";
 import Main from "./pages/Main";
 
 function App() {
   return (
     <GameContextProvider>
-      <Main />
+      <div className="flex flex-row">
+        <Sidebar />
+        <Main />
+      </div>
     </GameContextProvider>
   );
 }
--- src/pages/Main.tsx
+++ src/pages/Main.tsx
@@ -9,22 +9,21 @@ function Main() {
 
   return (
     <>
-      <div className="h-full w-full bg-gray-200 dark:bg-gray-900">
-        <div className="relative flex flex-row">
-          <Sidebar />
-          {/*Game List*/}
-          <div className="relative xl:grid-cols-4 lg:grid-cols-3 md:grid-cols-2 sm:grid-cols-2 grid w-full md:pt-0 pt-8 justify-center ">
-            {games.games.map((currentGame) => {
-              return (
+      <div className="bg-gray-200 dark:bg-gray-900 flex-1">
+        {/*Game List*/}
+        <div className="relative grid-cols-4 grid-rows-4 lg:grid-cols-3 md:grid-cols-2 sm:grid-cols-2 grid md:pt-0 pt-8 justify-center ">
+          {games.games.map((currentGame) => {
+            return (
+              <div onClick={() => console.log(currentGame)}>
                 <Game
                   id={currentGame.id}
                   Title={currentGame.title}
                   Genre={currentGame.genre}
                   ImageURL={currentGame.imageURL}
                 />
-              );
-            })}
-          </div>
+              </div>
+            );
+          })}
         </div>
       </div>
     </>
